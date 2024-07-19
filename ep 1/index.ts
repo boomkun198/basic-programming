d3630f8037c407be2ecb14b5f28abcd7ba45183c
boomkun198 <merakyou198@gmail.com>
2024-07-19 16:20:29 +0700

    สร้างโปรเจค

---
+++ ep 1/index.ts
@@ -0,0 +1,18 @@
+const firstname: string = 'traikun'
+let y: number = 1
+ y = 20
+let อยากกลับบ้านไหม : boolean = false 
+console.log(firstname)
+console.log(y)
+console.log(อยากกลับบ้านไหม)
+
+
+
+
+
+const a : number = 40
+const b : number = 20
+
+console.log(a!==b)
+console.log (a>b)
+console.log(a>=b)
